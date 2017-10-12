4674032cde724960b1a5b05ab9ced1aecb9d840e
Sumit <kumar1993sumit15@gmail.com>
2017-10-12 08:59:22 +0530

    fixed bool parser, Todo fix object & array parser

--- src/simple_json/core.clj
+++ src/simple_json/core.clj
@@ -35,26 +35,31 @@
     [nil input-str]
     )
   )
+(declare parse-values)
+(declare parse-array)
+(declare parse-object)
+(declare factory-parsers)
+
 (defn parse-null
   "Parses null values to nil"
   [input-str]
   (if (= "null" (subs input-str 0 4))
     [nil (subs input-str 4)]
-    input-str)
+    [nil input-str])
   )
 (defn parse-space
   "Removes whitespaces from left of strings"
   [input-str]
   (if (= \space (first input-str))
-    (cls/triml input-str)
-    input-str)
+    [nil cls/triml input-str]
+    [nil input-str])
   )
 (defn parse-comma
-  ""
+  "Ignores ',' & returns remaining string"
   [input-str]
   (if (= \, (first input-str))
-    (subs input-str 1)
-    input-str)
+    [nil (subs input-str 1)]
+    [nil input-str])
   )
 
 (defn parse-boolean
@@ -66,14 +71,7 @@
       :else [nil input-str]
       )
   )
-(defn parse-colon
-  "A value or object or array can be encountered after a colon"
-  [input-str]
-  (if (= \: (first input-str))
-    ;parse a value complete this method
-    [nil (subs input-str 1)]
-    )
-  )
+
 (defn parse-literal
   "Parses null true false"
   [input-str]
@@ -119,9 +117,60 @@
           )
     )
   )
+
+(defn parse-colon
+  "A value or object or array can be encountered after a colon"
+  [[x xs]]
+  (if (= \: x)
+    [nil xs]
+    [nil (str x xs)]
+    )
+  )
+
+(defn parse-array
+  [x xs]
+  (if (= x \[)
+    [(parse-values factory-parsers xs)]
+    )
+  )
+
+(defn parse-object
+  ""
+  [[x xs]]
+  (if (= x \{)
+    (let [[key rem] (parse-values factory-parsers xs)]
+      (let [[value remain] (parse-values factory-parsers rem)]
+        [(hash-map key value) remain]
+        )
+      )
+    [nil (str x xs)])
+  )
+
+(def factory-parsers (list parse-space parse-string parse-object parse-array parse-number parse-comma parse-colon ))
+
+(defn parse-values
+  "Parsing function called on where a value is expected like after ':' "
+  [[p & parsers] input-str]
+  (if false                                                      ;(or (empty? input-str) (empty? parsers))
+    nil
+    (let [[result rem] (p input-str)]
+      (if result
+        result
+        (parse-values parsers rem))
+      )
+    )
+  )
+
+(defn json-parse
+  "Parses the json string"
+  [input-string]
+  (parse-values factory-parsers input-string)
+  )
 ;; to be removed later
 (defn -main
   "I don't do a whole lot."
   []
   (println "Hello, World!"))
 (def my-str "{\"name\":\"John\", \"age\":null, \"city\":\"New York\" \"salary\": 500.55 \"exponential\": 2E20}")
+(def test-str "  {  \"name\":\"john\"}")
+(println (json-parse test-str))
